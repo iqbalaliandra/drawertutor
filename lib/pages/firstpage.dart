93705286a2ed9070d001c1846a83a4965d24ef8e
Iqbal Aliandra <iqbal.aliandra01@gmail.com>
2023-12-11 09:05:08 +0700

    final commit

---
+++ lib/pages/firstpage.dart
@@ -0,0 +1,61 @@
+import 'package:drawertutor/pages/secondpage.dart';
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple[300],
+        elevation: 0,
+        title: Center(
+          child: Text("D R A W E R"),
+        ),
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.deepPurple[100],
+          child: ListView(
+            children: [
+              DrawerHeader(
+                  child: Center(
+                child: Text(
+                  "L O G O",
+                  style: TextStyle(fontSize: 40),
+                ),
+              )),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text(
+                  "Page 1",
+                  style: TextStyle(fontSize: 20),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => FirstPage()));
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text(
+                  "Page 2",
+                  style: TextStyle(fontSize: 20),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => SecondPage()));
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+      backgroundColor: Colors.blue[200],
+      body: Center(
+        child: Text("F I R S T   P A G E"),
+      ),
+    );
+  }
+}
---
+++ lib/pages/secondpage.dart
@@ -0,0 +1,61 @@
+import 'package:drawertutor/pages/firstpage.dart';
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple[300],
+        elevation: 0,
+        title: Center(
+          child: Text("D R A W E R"),
+        ),
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.deepPurple[100],
+          child: ListView(
+            children: [
+              DrawerHeader(
+                  child: Center(
+                child: Text(
+                  "L O G O",
+                  style: TextStyle(fontSize: 40),
+                ),
+              )),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text(
+                  "Page 1",
+                  style: TextStyle(fontSize: 20),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => FirstPage()));
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text(
+                  "Page 2",
+                  style: TextStyle(fontSize: 20),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => SecondPage()));
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+      backgroundColor: Colors.green[200],
+      body: Center(
+        child: Text("S E C O N D   P A G E"),
+      ),
+    );
+  }
+}
